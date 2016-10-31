1bdc04d98c942f12dffbd0b8c397e48d1c355842
jieyuxu <jxu9@stuy.edu>
2016-10-30 21:23:27 -0400

    Update stat.c

--- stat.c
+++ stat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <time.h>
 
 int main() {
 	struct stat openfile;
@@ -10,6 +11,8 @@ int main() {
 	printf("File that has been statted:\tfile.file\n");
 	printf("File size:\t\t%d\n", openfile.st_size);
 	printf("File permissions:\t%o\n", openfile.st_mode);
-	printf("Time of access:\t\t%d\n", openfile.st_atim);
+	char date [100];
+	printf("%d\n", openfile.st_atim);
+	strcpy(date, ctime(openfile.st_atim));
+	printf("Time of access:\t\t%s\n", date);
 }
-
